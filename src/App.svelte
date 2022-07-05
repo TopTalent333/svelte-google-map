eb61ff431f638a1f8bc81ff12ea767614375eed8
TopTalent333 <oleg.livnenko.freelancer@gmail.com>
2022-07-05 16:24:00 +0000

    google map + svelte

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Map from './Map.svelte';
+	export let ready;
+</script>
+
+<svelte:head>
+	<script defer async
+	src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB5HYrOwNUgeMxMWUx3QGp8fev-PWjFoYw&callback=initMap">
+	</script>
+</svelte:head>
+
+<style>
+:global(body) {
+	padding: 0;
+}
+</style>
+
+{ #if ready }
+<Map></Map>
+{ /if }
---
+++ src/Map.svelte
@@ -0,0 +1,38 @@
+<script>
+	 import mapStyles from './map-styles'; // optional
+	 import jq from 'jquery';
+	let container;
+	let map;
+	let zoom = 8;
+    let center = {lat: -34.397, lng: 150.644};
+    
+    import { onMount } from 'svelte';
+    
+	onMount(async () => {
+		map = new google.maps.Map(container, {
+            zoom,
+			center,
+			styles: mapStyles // optional
+		});
+
+
+	});
+
+
+</script>
+
+<style>
+.full-screen {
+    width: 100vw;
+    height: 500px;
+}
+</style>
+
+<div class="full-screen" id="maparea" bind:this={container}>
+</div>
+
+<div>
+Location: <input type="text" id="us2-address" style="width: 200px"/>	
+Lat.: <input type="text" id="us2-lat"/>
+Long.: <input type="text" id="us2-lon"/>
+</div>
